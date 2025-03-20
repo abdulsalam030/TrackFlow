270320c595e578f3d5b040f309ebc39e71e26170
Abdulateef Abdulsalam <abdulsalamopeyemi030@gmail.com>
2025-03-20 11:14:43 +0100

    spinner added to the landing/login page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import Spinners from "../components/spinners";
 
 const Landing = () => {
+  const [loading, setLoading] = useState(false);
+
   const handleLogin = () => {
+    setLoading(true);
     const clientId = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
     const redirectUri = import.meta.env.VITE_SPOTIFY_REDIRECT_URI;
     const scopes =
       "user-top-read playlist-read-private user-read-recently-played";
-
     window.location.href = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&redirect_uri=${encodeURIComponent(
       redirectUri
     )}&scope=${encodeURIComponent(scopes)}`;
@@ -17,9 +20,10 @@ const Landing = () => {
       <h1 className="text-5xl font-bold mb-6">Welcome to TrackFlow</h1>
       <button
         onClick={handleLogin}
-        className="bg-green-500 text-black px-6 py-3 text-lg font-bold rounded-lg hover:bg-green-600"
+        className="bg-green-500 text-black px-6 py-2 text-lg font-bold rounded-lg hover:bg-green-600 flex items-center justify-center relative"
       >
         Login with Spotify
+        {loading && <Spinners />}
       </button>
     </div>
   );
